13e7aab2728f6cb6961c7e18706a327cbd5895e0
Natalia Łapińska (Czub) <82093201+nczub@users.noreply.github.com>
2024-09-02 11:48:12 +0200

    Update 2_QSAR.py

--- pages/2_QSAR.py
+++ pages/2_QSAR.py
@@ -87,7 +87,7 @@ st.markdown(
 #classification - threshold 1000 nM EC50
 # classification_model_path = 'mljar_AutoML_Compete_2024_08_29_23_38_44_1000' 
 #classification - threshold 10000 nM EC50
-# classification_model_path = 'mljar_AutoML_Compete_2024_08_29_21_37_05_10000' 
+classification_model_path = 'mljar_AutoML_Compete_2024_08_29_21_37_05_10000' 
 
 classification_model = AutoML(classification_model_path)
 calc = Calculator(descriptors, ignore_3D=True)
